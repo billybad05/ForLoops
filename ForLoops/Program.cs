9e5eafa033be912bb1bf48419cb1d51a11cf862b
Billy Dotson <billybad5@gmail.com>
2022-09-22 16:06:48 -0400

    Add project files.

---
+++ ForLoops/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hello, World!");
+/*
+int i = 1;
+while (i <= 5)
+{
+    Console.WriteLine(i);
+    i++;
+}
+*/
+int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+
+for(int i = 0; i < luckyNumbers.Length; i++)
+{
+    Console.WriteLine(luckyNumbers[i]);
+    
+}
